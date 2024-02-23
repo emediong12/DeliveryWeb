33ad05679b7ad971daac9e23dd4326b052e77a30
emediong12 <emediongblessed571@gmail.com>
2024-02-23 15:22:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+const header = document.querySelector('.header');
+const scrollLink = document.querySelectorAll('.navbar a:not(:last-child)');
+
+Array.from(scrollLink).map((link) => {
+  link.addEventListener('click', (e) => {
+    // Prevent Default
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop - 90;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth',
+    });
+    navbar.classList.remove('show');
+  });
+});
